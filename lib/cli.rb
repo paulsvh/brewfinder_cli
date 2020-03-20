51491ac556be27d597f04dede70954f76a588d10
Paul von Hagen <pvonhagen26@gmail.com>
2020-03-20 03:31:26 +0000

    added call and menu skeleton to cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,9 +1,17 @@
 class BrewfinderCli::CLI 
-  
+ 
+ 
   def call
-    puts "I'm in the cli"
+    puts "welcome message" 
+    sleep(2)
+    puts "input city to see breweries in city"
+    sleep (2)
+    puts "input exit to leave the app" 
+    sleep (2)
+    menu
   end
-  
-  
+    #use gsub to sub spaces in city names for an underscore
+  def menu
+    user_input = gets.strip.downcase
   
 end
