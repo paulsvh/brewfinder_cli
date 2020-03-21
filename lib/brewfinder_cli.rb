82361c0fa9a1c26787a40ba368bbf64a7238e335
paulsvh <pvonhagen26@gmail.com>
2020-03-20 19:56:22 -0700

    adjusted cli to reflect listing only LA breweries

--- lib/brewfinder_cli.rb
+++ lib/brewfinder_cli.rb
@@ -1,6 +1,6 @@
 #ENVIRONMENT FILE, REQUIRE ALL NECESSARY FILES AND RELATIVES HERE
-
+require 'pry'
 require_relative "./brewfinder_cli/version"
 require_relative '../lib/cli'
-require_relative '../lib/brewfinder'
+require_relative '../lib/brewery'
 require_relative '../lib/api'
--- lib/cli.rb
+++ lib/cli.rb
@@ -2,16 +2,46 @@ class BrewfinderCli::CLI
  
  
   def call
-    puts "welcome message" 
+    puts "Welcome thirsty friend!" 
     sleep(2)
-    puts "input city to see breweries in city"
+    puts "To view breweries on Los Angeles, enter 'view'"
     sleep (2)
-    puts "input exit to leave the app" 
+    puts "To leave the app, input 'exit'" 
     sleep (2)
     menu
   end
-    #use gsub to sub spaces in city names for an underscore
+    
   def menu
-    user_input = gets.strip.downcase
+    user_input = gets.to_i
+    if user_input == "view"
+      brewery_list
+      menu
+    elsif user_input == "exit"
+      goodbye
+    else
+      invalid
+    end
+  end
+
+  def invalid
+    puts "Invalid input, please try again!"
+    menu
+  end
+
+  def goodbye
+    puts "Goodbye!"
+    #exit the program
+  end
+
+  def brewery_list
+    #returns a numbered list of breweries from the api
+    puts "If you'd like more information on a brewery, enter it's number! If you'd like to exit, enter exit."
+  end
+
+  def selected_brewery
+    #iterate over all of the breweries and return the one that the user selected
+  end
+
+
   
 end
