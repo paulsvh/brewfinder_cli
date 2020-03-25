5325b2e52d7a6b518368f7cf668df13934c68765
paulsvh <pvonhagen26@gmail.com>
2020-03-24 19:26:09 -0700

    removed unused class variable

--- lib/brewfinder_cli/api.rb
+++ lib/brewfinder_cli/api.rb
@@ -1,8 +1,7 @@
 class API 
-  @@all_breweries = []
-  def self.get_breweries
-  	response = HTTParty.get('https://api.openbrewerydb.org/breweries?per_page=50&by_city=los_angeles')
-  	response.each {|hash| Brewery.new(hash)}  
-  end
+	def self.get_breweries
+  		response = HTTParty.get('https://api.openbrewerydb.org/breweries?per_page=50&by_city=los_angeles')
+  		response.each {|hash| Brewery.new(hash)}  
+  	end
   
 end
